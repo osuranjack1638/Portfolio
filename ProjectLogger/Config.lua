7b687202b850e5ba0ef51819d87e59d6b81cee64
osuranjack1638 <aslanuludag15@gmail.com>
2026-08-05 16:59:34 +0300

    Create Config.lua

---
+++ ProjectLogger/Config.lua
@@ -0,0 +1,12 @@
+-- Project Logger
+-- Copyright (c) 2026 osuranjack1638
+-- Licensed under the Apache License 2.0.
+-- See the LICENSE file in the project root for license information.
+
+local ProjectLoggerConfig = {}
+
+
+ProjectLoggerConfig.DisableLogging = false
+
+
+return ProjectLoggerConfig
